ff371f7a881604aad599ad7efdb8cfa04c2f5899
Daniel Moraes <daniel_diasm@hotmail.com>
2022-03-01 23:09:34 -0300

    added useful comments

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+# MicroPython MQTT PoC
+# Author: Daniel Z. Moraes (LeChevalier)
+# First Ver.: March 1st 2022
+# Description:
+# This is a PoC for a IoT using a M5 Atom Lite to control some target device.
+# M5 must be able to control the LED status via push button and MQTT, also updating
+# status on both places (Broker and RAM). With this proved, the update cand happen
+# also with any Pin. So this code can be customized by anyone.
+#
+# Note: Earlier this year I've made a version that connects to AWS, this is possible
+# as well, but for this PoC I've opted to keep it in my local network.
+# The MQTT Broker is in a RaspberryPi running Mosquitto.
+
 from machine import Pin as pin
 import network
 from neopixel import NeoPixel as neo_pixel
